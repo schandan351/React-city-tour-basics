126fc614d800c77185fcd43bc88cead020a067cf
chandan sharma <sharmachandan351@gmail.com>
2019-08-18 14:16:18 +0545

    basic setups and navbar added

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./navbar.scss";
+export default function NavBar(){
+  return(
+    <nav className="nav">  
+      <ul className="nav__container">  
+        <li ><a href="/" className="nav-links">City Tour</a></li>  
+        <li ><a href="/" className="nav-links">Home</a></li>  
+        <li ><a href="/" className="nav-links">Tours</a></li>  
+        <li ><a href="/" className="nav-links">Contact</a></li>  
+        <li><a href="/" className="nav-links">About</a></li>  
+      </ul>
+    </nav>
+  );
+}
